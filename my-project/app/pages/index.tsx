e3409d26916729fcf097eee447fd86aaf8aa2bf3
hasan <hasan>
2023-07-19 19:28:02 +0300

    changes

---
+++ my-project/app/pages/index.tsx
@@ -0,0 +1,9 @@
+import OnlyPage from "../components/onlyPage";
+
+export default function Home() {
+  return (
+    <main className={`bg-white text-black`}>
+      <OnlyPage />
+    </main>
+  );
+}
