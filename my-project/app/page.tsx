e949f66ab64b4991dd4c36a8d36c061b7211f785
hasan <hasan>
2023-07-20 10:11:14 +0300

    changes

---
+++ my-project/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { OnlyPage } from "./components/onlyPage";
+
+export default function Home() {
+  return (
+    <main className={`bg-white text-black`}>
+      <OnlyPage />
+    </main>
+  );
+}
